ad540fec62cbcba55be0253dc5b04ba8f5037a9f
Clayton-Stuart <72456578+Clayton-Stuart@users.noreply.github.com>
2025-04-30 15:25:53 -0400

    Add files via upload

---
+++ Examples/Parallel World/images/man.js
@@ -0,0 +1,12 @@
+var man = [
+  ["transparent", "black", "black", "black", "transparent"],
+  ["transparent", "black", "black", "black", "transparent"],
+  ["transparent", "black", "black", "black", "transparent"],
+  ["transparent", "transparent", "black", "transparent", "transparent"],
+  ["transparent", "black", "black", "black", "transparent"],
+  ["black", "transparent", "black", "transparent", "black"],
+  ["black", "transparent", "black", "transparent", "black"],
+  ["transparent", "transparent", "black", "transparent", "transparent"],
+  ["transparent", "black", "transparent", "black", "transparent"],
+  ["black", "transparent", "transparent", "transparent", "black"],
+];
