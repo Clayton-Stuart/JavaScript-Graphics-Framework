48f1045a657208a7bdb4977e2362311eb8a647f8
Clayton-Stuart <72456578+Clayton-Stuart@users.noreply.github.com>
2025-04-30 15:22:43 -0400

    Add files via upload

---
+++ Examples/2D World/Images/character.js
@@ -0,0 +1,8 @@
+var character = [
+  ["white", "white", "white", "white", "white", "white"],
+  ["white", "red", "white", "white", "red", "white"],
+  ["white", "red", "white", "white", "red", "white"],
+  ["white", "white", "white", "white", "white", "white"],
+  ["white", "red", "red", "red", "red", "white"],
+  ["white", "white", "white", "white", "white", "white"],
+];
